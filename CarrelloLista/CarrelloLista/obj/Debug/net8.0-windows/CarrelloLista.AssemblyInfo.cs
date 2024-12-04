cc97c1e377a9ce73c0d5e5e5ce5ada35efdcb9af
FUSCO GABRIELE <FUSCO.22376>
2024-12-04 12:26:44 +0100

    ecommerce

--- CarrelloLista/CarrelloLista/obj/Debug/net8.0-windows/CarrelloLista.AssemblyInfo.cs
+++ CarrelloLista/CarrelloLista/obj/Debug/net8.0-windows/CarrelloLista.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarrelloLista")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82f90951674b265ea8396406fde87de8134f1923")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarrelloLista")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarrelloLista")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
